05292d037dcae55d02e89eb5948c645227cbd15f
kingswanwho <chesterjchu@gmail.com>
2017-05-31 11:05:35 -0500

    Update

--- src/crawler.rs
+++ src/crawler.rs
@@ -8,7 +8,8 @@ use hyper::Client;
 use hyper::Url;
 use select::document::Document;
 use std::sync::{Arc, Mutex};
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
+use std::sync::mpsc::Sender;
 
 use function::*;
 use error::*;
@@ -16,16 +17,16 @@ use error::*;
 #[derive(Debug)]
 pub struct Crawler {
 	client: Client,
-	bank: Arc<Mutex<Vec<Url>>>,
-	queue: Arc<Mutex<VecDeque<Url>>>,
+	pub bank: Arc<Mutex<HashSet<Url>>>,
+	pub queue: Arc<Mutex<VecDeque<Url>>>,
 }
 
 impl Crawler {
-	pub fn new() -> Result<Crawler> {
+	pub fn new() -> Crawler {
 		let ssl = NativeTlsClient::new().unwrap();
 		let connector = HttpsConnector::new(ssl);
 		let client = Client::with_connector(connector);
-		let bank = Vec::new();
+		let bank = HashSet::new();
 		let deque: VecDeque<Url> = VecDeque::new();
 
 		let crawler = Crawler {
@@ -33,23 +34,23 @@ impl Crawler {
 			bank: Arc::new(Mutex::new(bank)),
 			queue: Arc::new(Mutex::new(deque)),
 		};
-		Ok(crawler)
+		crawler
 	}
 
-//////////==============================================\\\\\\\\\\\\
 	pub fn new_shared(queue: Arc<Mutex<VecDeque<Url>>>,
-					  bank: Arc<Mutex<Vec<Url>>>) -> Crawler {
-		let mut crawler = Crawler::new().unwrap();
+					  bank: Arc<Mutex<HashSet<Url>>>) -> Crawler {
+		let mut crawler = Crawler::new();
 		crawler.queue = queue;
 		crawler.bank = bank;
 		crawler
 	}
-//////////==============================================\\\\\\\\\\\\
 
-	pub fn bank(&self) -> Arc<Mutex<Vec<Url>>> {
+	#[allow(dead_code)]
+	pub fn bank(&self) -> Arc<Mutex<HashSet<Url>>> {
 		self.bank.clone()
 	}
 
+	#[allow(dead_code)]
 	pub fn queue(&self) -> Arc<Mutex<VecDeque<Url>>> {
 		self.queue.clone()
 	}
@@ -57,11 +58,6 @@ impl Crawler {
 	pub fn check_empty(&self) -> bool {
 		let queue = lock(&(self.queue)).unwrap();
 		queue.is_empty()
-	}
-
-	pub fn len(&self) -> usize {
-		let queue = lock(&self.queue).unwrap();
-		queue.len()
 	}
 
 	pub fn pop(&self) -> Option<Url> {
@@ -72,62 +68,111 @@ impl Crawler {
 		sync_add_url(&self.queue, &self.bank, url)
 	}
 
-	pub fn parse_url(&self) {
-		/*
-		while !self.check_empty() {
-			let length = self.len();
-
-			let (tx, rx) = mpsc::channel::<T: Url>();
-			for _ in 0..length {
-				let tx = tx.clone();
-
-				thread::spawn(move || {
-					// let url = match self.pop() {
-					// 	Some(url) => url,
-					// 	None => continue,
-					// };
-					let url = self.pop().unwrap();
-					let (url, body) = crawler(url).unwrap();
-					let body = String::from_utf8_lossy(body.as_slice()).to_string();
-					let doc = Document::from(body.as_str());
-					let hrefs = scrap_href(&doc, "href");
-					for href in hrefs {
-						if href.starts_with('#') {
-							continue;
-						}
-						let url = match convert_url(&url, &href) {
-							Some(u) => u,
-							None => continue,
-						};
-						self.push(url.clone());
-						//println!("{:?}", url);
-					}
-				});
+	pub fn parse(&self, url: Url, tx: Sender<Vec<Url>>, id: usize) -> Result<(Url)> {
+		println!("Crawling {:?}", url);
+		let (url, body) = match crawler(url) {
+			Ok(t) => t,
+			Err(_) => bail!(ErrorKind::CannotParse),			
+		};
+		let body = String::from_utf8_lossy(body.as_slice()).to_string();
+		let doc = Document::from(body.as_str());
+		let mut urls = Vec::new();
+		let hrefs = scrap_href(&doc, "href");
+		for href in hrefs {
+			if href.starts_with('#') {
+				continue;
 			}
-			println!("{:?}", rx.recv().unwrap());
-		}
-		*/
-
-		while !self.check_empty() {
-			let url = match self.pop() {
-				Some(url) => url,
+			let url = match convert_url(&url, &href) {
+				Some(u) => u,
 				None => continue,
 			};
-			let (url, body) = crawler(url).unwrap();		
-			let body = String::from_utf8_lossy(body.as_slice()).to_string();
-			let doc = Document::from(body.as_str());
-			let hrefs = scrap_href(&doc, "href");
-			for href in hrefs {
-				if href.starts_with('#') {
-					continue;
-				}
-				let url = match convert_url(&url, &href) {
-					Some(u) => u,
-					None => continue,
-				};
-				self.push(url.clone());
-				println!("{:?}", url);
-			}
+			println!("To Crawl {}: {:?}", id, url);
+			urls.push(url);
 		}
+		match tx.send(urls.clone()) {
+			Ok(_) => {}
+			Err(_) => panic!("Cannot send the result to channel!"),
+		}
+		println!("==========Finish {} thread==========", id);
+		Ok(url)
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn crawler_set() {
+		let crawler = Crawler::new();
+		let queue = lock(&crawler.queue).unwrap();
+		assert_eq!(queue.len(), 0);
+		let bank = lock(&crawler.bank).unwrap();
+		assert_eq!(bank.len(), 0);
+	}
+
+	#[test]
+	fn test_new_shared() {
+		let mut bank = HashSet::new();
+		let mut deque: VecDeque<Url> = VecDeque::new();
+
+		let url1 = Url::parse("https://www.yahoo.com").unwrap();
+		let url2 = Url::parse("https://www.google.com").unwrap();
+		let url3 = Url::parse("https://www.youtube.com").unwrap();
+		let url4 = Url::parse("https://www.facebook.com").unwrap();
+
+		bank.insert(url1.clone());
+		bank.insert(url2.clone());
+		bank.insert(url3.clone());
+		bank.insert(url4.clone());
+		deque.push_back(url1);
+		deque.push_back(url2);
+		deque.push_back(url3);
+		deque.push_back(url4);
+
+		let b = Arc::new(Mutex::new(bank));
+		let d = Arc::new(Mutex::new(deque));
+
+		let crawler = Crawler::new_shared(d, b);
+		let dequeue = lock(&crawler.queue).unwrap();
+		let hashset = lock(&crawler.bank).unwrap();
+		assert_eq!(dequeue.len(), 4);
+		assert_eq!(hashset.len(), 4);
+	}
+
+	#[test]
+	fn test_check_empty() {
+		let crawler = Crawler::new();
+		assert_eq!(crawler.check_empty(), true);
+
+		let url1 = Url::parse("https://www.yahoo.com").unwrap();
+		let url2 = Url::parse("https://www.google.com").unwrap();
+		crawler.push(url1).unwrap();
+		crawler.push(url2).unwrap();
+		assert_eq!(crawler.check_empty(), false);
+	}
+
+	#[test]
+	fn test_push() {
+		let crawler = Crawler::new();
+		let url1 = Url::parse("https://www.yahoo.com").unwrap();
+		let url2 = Url::parse("https://www.google.com").unwrap();
+		crawler.push(url1).unwrap();
+		crawler.push(url2).unwrap();
+		let queue = lock(&crawler.queue).unwrap();
+		let bank = lock(&crawler.bank).unwrap();
+		assert_eq!(queue.len(), 2);
+		assert_eq!(bank.len(), 2);
+	}
+
+	#[test]
+	fn test_pop() {
+		let crawler = Crawler::new();
+		let url1 = Url::parse("https://www.yahoo.com").unwrap();
+		let url2 = Url::parse("https://www.google.com").unwrap();
+		crawler.push(url1.clone()).unwrap();
+		crawler.push(url2.clone()).unwrap();
+		assert_eq!(crawler.pop().unwrap(), url1);
+		assert_eq!(crawler.pop().unwrap(), url2);
 	}
 }
